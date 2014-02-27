9afea9c1f73f2f5d6fe5958db6a7fe1f77dd1dea
renefs87 <renefs87@gmail.com>
2014-02-27 21:14:10 +0100

    Added contact form

--- renefernandezcom/renefernandezcom/urls.py
+++ renefernandezcom/renefernandezcom/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from renefernandezcom.views import *
+from captcha import urls
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,5 +10,6 @@ urlpatterns = patterns('',
     url(r'^$', 'renefernandezcom.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     (r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^captcha/', include('captcha.urls')),
 )
--- renefernandezcom/renefernandezcom/views.py
+++ renefernandezcom/renefernandezcom/views.py
@@ -1,6 +1,35 @@
+# -*- coding: utf-8 -*-
 __author__ = 'rene'
 
 from django.shortcuts import render
 
+from django.utils.translation import ugettext as _
+
+from django.core.mail import send_mail
+from renefernandezcom.forms import ContactForm
+
 def home(request):
-    return render(request, 'home.html')
+    form = contact(request)
+
+    return render(request, 'home.html', {'form': form})
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            send_mail(
+                '[Página personal] Formulario de contacto',
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['hola@renefernandez.com'],
+            )
+
+            new_form = ContactForm()
+            new_form.success = _('El mensaje ha sido enviado correctamente. ¡Gracias!')
+
+            return new_form
+    else:
+        form = ContactForm()
+    return form
