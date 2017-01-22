4a421ba780b06cad8b972206297dd103ed2c87f8
René Fernández <renefs87@gmail.com>
2017-01-22 14:09:00 +0100

    Remove captcha urls

--- web/renefernandez_com/urls.py
+++ web/renefernandez_com/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('renefernandez_com.apps.main.urls')),
     url(r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^captcha/', include('captcha.urls')),
 ]
