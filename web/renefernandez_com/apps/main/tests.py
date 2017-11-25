4666a9b4bf383d0d6d74cdf7dfde70e4f96b8ac3
René Fernández <renefs87@gmail.com>
2017-11-26 00:12:47 +0100

    Remove unused tests

--- web/renefernandez_com/apps/main/tests.py
+++ web/renefernandez_com/apps/main/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
 
 # Create your tests here.
+from django.urls import reverse
+
+
+class HomeTestCase(TestCase):
+    client = None
+
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_home(self):
+        # Simple get
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
