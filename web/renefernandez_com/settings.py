a628016559999c88fb190c03d05a72265c492a2a
René Fernández <renefs87@gmail.com>
2018-05-13 13:06:29 +0200

    Add logging to settings

--- web/renefernandez_com/settings.py
+++ web/renefernandez_com/settings.py
@@ -124,6 +124,44 @@ LOCALE_PATHS = (
     os.path.join(BASE_DIR, "locale"),
 )
 
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s | %(asctime)s | %(name)s | %(module)s | %(funcName)s():%(lineno)d :  %(message)s'
+            # 'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s',
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': config.LOG_DEBUG_PATH,
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'WARNING',  # DEBUG displays db queries
+            'propagate': True,
+        },
+        'webapp': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG'
+        }
+    },
+}
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
